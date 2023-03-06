0de9274df922a56e1bf30620e4e6ee3c07b2b073
fdai2003 <fdai2003@gmail.com>
2023-03-06 17:09:55 +0100

    add Class

---
+++ src/main/java/de/bsohef/programming/java/examples/hello/HelloStudent.java
@@ -0,0 +1,7 @@
+package de.bsohef.programming.java.examples.hello;
+
+public class HelloStudent {
+    public static void main(String[] args) {
+        System.out.println("Hello Student");
+    }
+}
